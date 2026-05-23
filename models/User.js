247378b8dd5c7d304b28b7445231a3dbefbfc3c7
unknown <ehijearthur@gmail.com>
2026-05-23 13:20:02 +0100

    Added user model

---
+++ models/User.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const userSchemma = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, 'Please add a username'],
+        trim: true,
+        unique: true,
+    },
+
+    email: {
+        type: String,
+        required: [true, 'Please an email'],
+        unique: true,
+        lowercase: true,
+        trim: true,
+        match: [
+            /^\w+([\.-]?\w+)*@w+([\.-]?\w+)*(\.\w{2,3})+$/,
+            'Please add a vaild email'
+        ]
+    },
+
+    password: {
+        type: String,
+        required: [true, 'Please add a password'],
+        minlength: 6,
+        select: false,
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model('User', userSchema);
