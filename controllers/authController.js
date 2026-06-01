fc7273c9389d66703e81b26591120cea12d50d27
unknown <ehijearthur@gmail.com>
2026-06-01 08:52:47 +0100

    fixed bugs

--- controllers/authController.js
+++ controllers/authController.js
@@ -61,7 +61,7 @@ exports.loginUser = async (req, res) => {
 
     try {
         const { email, password } = req.body;
-        const user = await User.findOne({ email }).select("password");
+        const user = await User.findOne({ email }).select("+password");
 
         if (!user) {
             return  res.status(400).json({
